5965dbbaaef6604ba710d9b90777b5e9e136f015
Karl K <Karlk15@ru.is>
2017-03-30 17:40:21 +0000

    player dies if touch ground

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -7,7 +7,7 @@ window.Player = (function() {
 	// for 1024x576px canvas.
 	var SPEED = 35; // * 10 pixels per second
 	var WIDTH = 5;
-	var HEIGHT = 5;
+	var HEIGHT = 10;
 	var INITIAL_POSITION_X = 30;
 	var INITIAL_POSITION_Y = 10;
 
