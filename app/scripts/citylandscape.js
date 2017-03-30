67e6d34b72a3e577ad338173d54fc220d4711a82
Oli Olafsson <Oli Olafsson>
2017-03-30 17:27:31 +0000

    background ready!

---
+++ app/scripts/citylandscape.js
@@ -0,0 +1,23 @@
+window.CityLandscape = (function() {
+    'use strict';
+
+    var SPEED = 4;
+
+    var CityLandscape = function(el, game) {
+        this.el = el;
+        this.game = game;
+        this.pos = { x: 0, y: 0};
+    };
+
+    CityLandscape.prototype.onFrame = function(delta) {
+        this.pos.x -= delta * SPEED;
+
+        if(this.pos.x <= -300) {
+            this.pox.x = 0;
+        }
+
+        this.el.css('transform', 'translateZ(0) translate(' + this.pox.x + 'em, ' + this.pos.y + 'em)');
+    };
+    return CityLandscape;
+
+})();
